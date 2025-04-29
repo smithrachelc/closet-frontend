ee91374f01d45a772846dc718e81dfe91fadc1e1
Rachel Smith <smithrachelc@gmail.com>
2025-04-29 08:49:07 -0500

    new frontend pt 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,7 @@ import { RouterModule } from '@angular/router';
 @Component({
   standalone: true,
   selector: 'app-root',
-  template: '<router-outlet></router-outlet>',
+  template: `<router-outlet></router-outlet>`,
   imports: [RouterModule]
 })
 export class AppComponent {}
