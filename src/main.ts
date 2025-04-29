2f0c2a258db103222a853f6a0fcf837ae612df65
Rachel Smith <smithrachelc@gmail.com>
2025-04-29 08:51:55 -0500

    new frontend pt 3

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { AppComponent } from './app/app.component';
 import { appRoutes } from './app/app.routes';
 import { provideRouter } from '@angular/router';
+import { provideHttpClient } from '@angular/common/http';
 
 bootstrapApplication(AppComponent, {
-  providers: [provideRouter(appRoutes)],
+  providers: [
+    provideRouter(appRoutes),
+    provideHttpClient() // ✅ THIS FIXES IT
+  ]
 });
