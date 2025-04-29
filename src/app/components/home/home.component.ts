54fa95e5c144226ca3a1cdafbd42a58008c715fe
Rachel Smith <smithrachelc@gmail.com>
2025-04-29 11:35:40 -0500

    changed style

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./home.component.css']
 })
 export class HomeComponent {
+  constructor(private router: Router) {}
+
   alertFeature() {
-    alert('Feature coming soon!');
+    this.router.navigate(['/signup']);
   }
 }
